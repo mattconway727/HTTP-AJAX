66e579e331ae246fccc1067b15eec36f01b4dd0e
Matt Conway <mattconway727@gmail.com>
2019-06-17 22:20:15 -0400

    completed Friends.js and FriendContainer.js - MVP complete

---
+++ friends/src/FriendContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Friends from "./Friends";
+import "./App.css";
+
+class FriendContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+    // console.log('hi', this.state.friends)
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        // console.log(res)
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(err => {
+        console.log("Gotcha", err);
+      });
+  }
+  render() {
+    return (
+      <div className="friendContainer">
+        <Friends friends={this.state.friends} />
+      </div>
+    );
+  }
+}
+
+export default FriendContainer;
